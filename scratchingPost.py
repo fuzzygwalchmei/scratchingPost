a2bd31db835fc49ac77d2d0fcabbc670213fc6db
fuzzygwalchmei <marc.falzon@gmail.com>
2019-03-21 13:13:39 +1100

    first tkinter commit

--- scratchingPost.py
+++ scratchingPost.py
@@ -1,2 +1,12 @@
-myName = input("What is your name?: ")
-print("Hi, ",myName)
+import tkinter as tk
+import  tkinter.scrolledtext as tkst
+
+myWindow = tk.Tk()
+myWindow.geometry('500x500')
+
+btnFirst = tk.Button(myWindow, text="First Button").pack(fill='both')
+btnSecond = tk.Button(myWindow, text="Second Button").pack(fill='both')
+btnThird = tk.Button(myWindow, text="Third Button").pack(fill='both')
+
+txtWindow = tkst.ScrolledText(myWindow, height=20).pack(fill='both')
+myWindow.mainloop()
