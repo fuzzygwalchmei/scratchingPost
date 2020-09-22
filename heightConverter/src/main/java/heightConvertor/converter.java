ae033a0c31f2b2877754d0fa6845979ae4e049f6
fuzzygwalchmei <marc.falzon@gmail.com>
2020-09-22 14:08:35 +1000

    Updated converter.java

--- heightConverter/src/main/java/heightConvertor/converter.java
+++ heightConverter/src/main/java/heightConvertor/converter.java
@@ -2,15 +2,41 @@ package heightConvertor;
 
 public class converter {
 
-	private int feet;
-	private int inches;
 	
-	public int converter(int feet, int inches) {
-		this.feet = feet;
+	private int inches;
+	private int cm;
+	private int kg;
+	private int[] converted;
+	
+	public int length_imp(int feet, int inches) {
 		this.inches = inches;
 		
-		int cm = (int) (((feet * 12) + inches) * 2.54);
+		cm = (int) (((feet * 12) + inches) * 2.54);
 		return cm;
 		
+	}
+	
+	public int weight_imp(int pound, int stone) {
+		kg = (int) (((stone *14) + pound) * 2.2046226218);
+		return kg;
+	}
+	
+	public int[] length_met(int cm) {
+		this.cm = cm;
+		inches = (int) (cm * 0.393701);
+		converted[0] = inches / 12;
+		converted[1] = inches%12;
+		return converted;
+				
+	}
+	
+	public int[] weight_met(int kg) {
+		// 6.35kg == 1 stone == 14 pound == 16 ounces
+		this.kg = kg;
+		int grams = (int)(kg / 0.02835);
+		converted[0] = grams/224;
+		converted[1] = (grams - converted[0] * 244)%14;
+		converted[2] = grams - (converted[0] * 224) - (converted[1] * 14);
+		return converted;
 	}
 }
