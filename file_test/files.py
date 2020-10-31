f88374fc3a681a4d8a8c2006b606923be729c2c9
fuzzygwalchmei <marc.falzon@gmail.com>
2020-10-31 15:58:59 +1100

    base setup for file

---
+++ file_test/files.py
@@ -0,0 +1,6 @@
+
+
+
+
+def face():
+    pass
---
+++ file_test/test_files.py
@@ -0,0 +1,26 @@
+import pytest
+import os
+import files
+
+
+
+def test_file_create():
+    pass
+
+
+
+def test_file_open():
+    pass
+
+
+
+def test_file_read():
+    info = files.test_file_read(filename)
+    assert info[0] == "Line 1"
+
+
+
+def test_file_write():
+    pass
+
+
