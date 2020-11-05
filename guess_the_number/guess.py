e505790ed6238055bda0e1bbdfd55dea0172ef59
fuzzygwalchmei <marc.falzon@gmail.com>
2020-11-05 16:24:10 +1100

    abstracting guess the number

--- guess_the_number/guess.py
+++ guess_the_number/guess.py
@@ -1,15 +1,23 @@
 from random import randint
 
-num = randint(1, 101)
+finished = False
 guess = 0
 guesses = 0
 
-while guess != num:
+def gen_rand_number():
+    return randint(1, 101)
+
+def check_number(guess):
+    return guess == num
+
+
+while not finished:
     guess = int(input(f"Please guess a number between 1 and 100 (guess {guesses}): "))
     guesses += 1
+
     if guess not in range(1,101):
         print("That number was outside of the range, its only between 1 and 100\n")
-    elif guess == num:
+    elif check_number:
         if guesses == 1:
             print("Wow, you guessed it on the first go!")
             break
