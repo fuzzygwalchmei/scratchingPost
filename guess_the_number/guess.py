373dda59090c5161f7b3681c478eb88b1e8d04ff
fuzzygwalchmei <marc.falzon@gmail.com>
2020-11-05 16:36:46 +1100

    Small updated to guess. Fixing errors, more abstr

--- guess_the_number/guess.py
+++ guess_the_number/guess.py
@@ -4,31 +4,40 @@ finished = False
 guess = 0
 guesses = 0
 
+
 def gen_rand_number():
     return randint(1, 101)
 
 def check_number(guess):
     return guess == num
 
+def advise_user():
+        if guess < num:
+            print("That guess was too low, try a higher number.\n")
+        if guess > num:
+            print("That guess was too high, try a lower number.\n")
+
+num = gen_rand_number()
 
 while not finished:
-    guess = int(input(f"Please guess a number between 1 and 100 (guess {guesses}): "))
+    try:
+        guess = int(input(f"Please guess a number between 1 and 100 (guess {guesses}): "))
+    except:
+        print("Im going to guess you didnt enter a number")
+
     guesses += 1
 
     if guess not in range(1,101):
-        print("That number was outside of the range, its only between 1 and 100\n")
-    elif check_number:
+        print("That guess was outside of the range, its only between 1 and 100\n")
+    elif check_number(guess):
+        finished == True
         if guesses == 1:
             print("Wow, you guessed it on the first go!")
             break
         else:
             print(f"Congrats, you guessed the number in only {guesses} guesses!")
-    elif guess < num:
-        print("That guess was too low, try a higher number.\n")
-    elif guess > num:
-        print("That guess was too high, try a lower number.\n")
     else:
-        print("Not sure how we even got here as it shouldnt be possible\n")
+        advise_user()
 
 
 
