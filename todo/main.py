f94df7d589b85fd760c1780ed3cbef4947510427
fuzzygwalchmei <marc.falzon@gmail.com>
2020-11-09 19:28:52 +1100

    added save and load functions to ToDo

--- todo/main.py
+++ todo/main.py
@@ -5,8 +5,8 @@ class todo_item():
         self.note = note
             
     def __str__(self):
-        return f"""ID: {self.id}\n 
-        Subject: {self.subject} \n 
+        return f"""ID: {self.id}
+        Subject: {self.subject}
         Note: {self.note}"""
 
     
@@ -23,3 +23,17 @@ class todo_list():
         for item in self.store:
             print(item)
 
+    def save(self):
+        filename = 'Todo_list.txt'
+        with open(filename, 'w') as f:
+            for item in self.store:
+                f.writelines(f'{item.id},{item.subject},{item.note}\n')
+
+    def load(self):
+        filename = 'Todo_list.txt'
+        with open(filename, 'r') as f:
+            info = f.readlines()
+        
+        for item in info:
+            id, subject, note = item.split(',')
+            self.store.append(todo_item(id, subject, note))
