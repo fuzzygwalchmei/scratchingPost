cb36eab9e671ec1a72a9c9c4aabca9d9369cb282
fuzzygwalchmei <marc.falzon@gmail.com>
2020-11-15 11:49:25 +1100

    added display all for todos as well as html template

--- todo/app.py
+++ todo/app.py
@@ -1,14 +1,34 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template
 from flask_sqlalchemy import SQLAlchemy
 #from flask-RESTful import Resource, Api 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.sqlite3'
 db = SQLAlchemy(app)
+
+class ToDo(db.Model):
+    __tablename__ = 'todos'
+
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String)
+    note = db.Column(db.String)
+
+    def __init__(self, subject, note):
+        self.subject = subject
+        self.note = note
+
+    def __repr__(self):
+        return f'<ToDo(id: {self.id} - note: {self.note}'
+db.create_all()
 
 @app.route('/')
 def home():
     return "<h1> Hello World </h1>"
+
+@app.route('/all')
+def all():
+    todos = ToDo.query.all()
+    return render_template('show_stuff.html', todos = todos)
 
 
 if __name__ == "__main__":
