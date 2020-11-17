c2539b6fa9f9bec42fb6d958a4c485d5e3c0287a
fuzzygwalchmei <marc.falzon@gmail.com>
2020-11-17 16:46:47 +1100

    created db.py as a flask api sqlalchemy base

--- todo/db.py
+++ todo/db.py
@@ -1,19 +1,63 @@
 import sqlalchemy
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource
+from flask_marshmallow import Marshmallow
+
+app=Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.sqlite3'
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class ToDo(db.Model):
+    __tablename__ = 'todos'
+
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String)
+    note = db.Column(db.String)
+
+    def __init__(self, subject, note):
+        self.subject = subject
+        self.note = note
+
+    def __repr__(self):
+        return f'<ToDo(id: {self.id} - note: {self.note}'
+db.create_all()
+
+class TodoSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "subject", "note")
+        model = ToDo
+    
+todo_schema = TodoSchema()
+todos_schema = TodoSchema(many=True)
+
+
+
 
 def get_connection():
     pass
 
-def get_all():
-    pass
 
-def get_single():
-    pass
+class TodoAPI(Resource):
+    def get(self):
+        todos = ToDo.query.all()
+        return todos_schema.dump(todos)
 
-def add_item():
-    pass
+    def get_single(self, id):
+        pass
 
-def update_item():
-    pass
+    def add_item(self, id):
+        pass
 
-def delete_item():
-    pass
+    def update_item(self, id):
+        pass
+
+    def delete_item(self, id):
+        pass
+
+api.add_resource(TodoAPI, '/todo/api/v1.0/todos', endpoint = 'todos')
+
+if __name__ == "__main__":
+    app.run(debug=True)
