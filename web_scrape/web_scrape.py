7bba43b331f8cead0a0675b4aa886251a6e17114
fuzzygwalchmei <marc.falzon@gmail.com>
2020-11-20 17:11:46 +1100

    scrape page and follow page and gets details

--- web_scrape/web_scrape.py
+++ web_scrape/web_scrape.py
@@ -1,11 +1,38 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-r = requests.get('https://www.jucktion.com/forum/udemy-coupon/')
 
-webpage = bs(r.content)
+courses = {}
 
-# This will get the links of each of the articles, doesnt cater for next page yet
-pages = webpage.select('td.subject a')
-for page in pages:
-    print(page['href'])
+def get_udemy_links():
+    r = requests.get('https://www.jucktion.com/forum/udemy-coupon/')
+    webpage = bs(r.content, features="html.parser")
+    # This will get the links of each of the articles, doesnt cater for next page yet
+    pages = webpage.select('td.subject a')
+
+    udemy_links = []
+
+    for page in pages[1:]:
+        link = requests.get(page['href'])
+        linkpage = bs(link.content, features="html.parser")
+        coupon = linkpage.select('a.bbc_link')
+        bbc_link = coupon[0]['href']
+        udemy_links.append(bbc_link)
+    print('pages completed')
+    return udemy_links
+
+def get_course_info(link):
+    r = requests.get(link)
+    page = bs(r.content, features="html.parser")
+    course = {}
+    course['title
+    '] = page.find('h1',{'data-purpose':'lead-title'}).text.strip()
+    course['length'] = page.find('span',{'data-purpose':'video-content-length'}).text
+    print(course)
+    return course
+
+udemylinks = get_udemy_links()
+for link in udemylinks:
+    courses[link] = get_course_info(link)
+
+print(courses)
