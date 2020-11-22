210f6b9c6b329621d25e91e4e1a14521298f73ef
fuzzygwalchmei <marc.falzon@gmail.com>
2020-11-22 16:54:02 +1100

    changed minor page logic and added named tuple

--- web_scrape/web_scrape.py
+++ web_scrape/web_scrape.py
@@ -1,8 +1,16 @@
 import requests
 from bs4 import BeautifulSoup as bs
+from collections import namedtuple
 
+Course = namedtuple('Course',['title', 'link', 'length', 'posted'])
+courses = []
 
-courses = {}
+def get_course_info(link):
+    r = requests.get(link)
+    page = bs(r.content, features="html.parser")
+    title = page.find('h1',{'data-purpose':'lead-title'}).text.strip()
+    length = page.find('span',{'data-purpose':'video-content-length'}).text
+    return title, length
 
 def get_udemy_links():
     base = 'https://www.jucktion.com/forum/udemy-coupon/'
@@ -11,7 +19,7 @@ def get_udemy_links():
     count = 00
     pages = []
     # This will get the links of each of the articles, doesnt cater for next page yet
-    while 'Today' in last_date or 'Yesterday' in last_date:
+    while any([x in last_date for x in ['Today', 'Yesterday']]) :
         r = requests.get(base+f'{count:02}')
         webpage = bs(r.content, features="html.parser")
         last_date = webpage.select('td.lastpost')[-1].text
@@ -21,30 +29,22 @@ def get_udemy_links():
 
     udemy_links = []
     
-    print(pages[0])
     for page in pages[1:]:
         link = requests.get(page['href'])
         linkpage = bs(link.content, features="html.parser")
         coupon = linkpage.select('a.bbc_link')
-        print(coupon)
         bbc_link = coupon[0]['href']
         posted = linkpage.select('div.smalltext')[0].text
-        udemy_links.append((bbc_link, posted))
-        print(f'{bbc_link} - Done')
+        # udemy_links.append((bbc_link, posted))
+        title, length = get_course_info(bbc_link)
+        courses.append(Course(title, bbc_link, length, posted))
+        print(f'{title} - Done')
     print('pages completed')
 
     return udemy_links
 
-def get_course_info(link):
-    r = requests.get(link)
-    page = bs(r.content, features="html.parser")
-    course = {}
-    course['title'] = page.find('h1',{'data-purpose':'lead-title'}).text.strip()
-    course['length'] = page.find('span',{'data-purpose':'video-content-length'}).text
-    return course
+
 
 udemylinks = get_udemy_links()
-for link in udemylinks:
-    courses[link] = get_course_info(link[0])
 
 print(courses)
