a77bb8e7c67340e4f34888b77a0ea57dadd558bc
fuzzygwalchmei <marc.falzon@gmail.com>
2020-11-22 17:03:29 +1100

    functionalised and cleaning on web scraper

--- web_scrape/web_scrape.py
+++ web_scrape/web_scrape.py
@@ -12,12 +12,23 @@ def get_course_info(link):
     length = page.find('span',{'data-purpose':'video-content-length'}).text
     return title, length
 
+def get_course_details(page):
+    link = requests.get(page['href'])
+    linkpage = bs(link.content, features="html.parser")
+    coupon = linkpage.select('a.bbc_link')
+    bbc_link = coupon[0]['href']
+    posted = linkpage.select('div.smalltext')[0].text
+    # udemy_links.append((bbc_link, posted))
+    title, length = get_course_info(bbc_link)
+    courses.append(Course(title, bbc_link, length, posted))
+    print(f'{title} - Done')
+
 def get_udemy_links():
     base = 'https://www.jucktion.com/forum/udemy-coupon/'
-    
     last_date = 'Today'
     count = 00
     pages = []
+    
     # This will get the links of each of the articles, doesnt cater for next page yet
     while any([x in last_date for x in ['Today', 'Yesterday']]) :
         r = requests.get(base+f'{count:02}')
@@ -27,18 +38,10 @@ def get_udemy_links():
         print(f'Finished pages for {count:02}')
         count += 20
 
-    udemy_links = []
+    # udemy_links = []
     
     for page in pages[1:]:
-        link = requests.get(page['href'])
-        linkpage = bs(link.content, features="html.parser")
-        coupon = linkpage.select('a.bbc_link')
-        bbc_link = coupon[0]['href']
-        posted = linkpage.select('div.smalltext')[0].text
-        # udemy_links.append((bbc_link, posted))
-        title, length = get_course_info(bbc_link)
-        courses.append(Course(title, bbc_link, length, posted))
-        print(f'{title} - Done')
+        get_course_details(page)
     print('pages completed')
 
     return udemy_links
