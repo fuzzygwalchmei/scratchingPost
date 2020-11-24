085f6535d449fb16c3c434602912e80c8f89f0ea
fuzzygwalchmei <marc.falzon@gmail.com>
2020-11-24 17:32:05 +1100

    added notes

--- HKM_takehome/takehome.py
+++ HKM_takehome/takehome.py
@@ -17,16 +17,18 @@ print(f'Sys.argv: {handle}')
 def base_scrape(handle):
     print(f'start: {handle}')
     print(BASE+handle)
-    r = requests.get(BASE+handle, headers={'Authorization':f'Bearer {BEARER_TOKEN}'})
+    r = requests.get(BASE+handle+'&f=live', headers={'Authorization':f'Bearer {BEARER_TOKEN}'})
 
     page = json.loads(r.content)
     try:
-        print(page['statuses'][0]['text'])
+        for i in range(15):
+            print(page['statuses'][i]['text'])
     except:
-        print('Nothing in range')
+        print('Nothing New')
 
-    # soup = bs(r.content, features="html.parser")
-    # print(soup.find_all('div',attrs={'data-testid':'tweet'}))
 
 base_scrape(handle)
 
+# api stream/rules where authorid  == supplied handle?
+# get user id from supplied handle
+# store in some sort of list/dict? compare timestamps or tweet id, only print new ones
