be9ca8a1efab4fb92c814d0802a14afb8aa02f63
fuzzygwalchmei <marc.falzon@gmail.com>
2020-11-24 15:01:52 +1100

    added twitter API to take home

--- HKM_takehome/takehome.py
+++ HKM_takehome/takehome.py
@@ -1,12 +1,25 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import sys
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
-BASE = 'https://twitter.com/'
+
+
+BASE = 'https://api.twitter.com/1.1/search/tweets.json?q='
+handle = sys.argv[1]
+BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
+
+print(f'Sys.argv: {handle}')
 
 def base_scrape(handle):
     print(f'start: {handle}')
-    r = requests.get(BASE+handle)
-    soup = bs(r.content, features="html.parser")
-    print(soup.find('div',{'data-testid':'tweet'}))
+    print(BASE+handle)
+    r = requests.get(BASE+handle, headers={'Authorization':f'Bearer {BEARER_TOKEN}'})
+    print(r.content[0])
+    # soup = bs(r.content, features="html.parser")
+    # print(soup.find_all('div',attrs={'data-testid':'tweet'}))
 
-base_scrape('fuzzygwalchmei')
+base_scrape(handle)
+
